167c9a8f24c7fbb32c1b36a357ee9a5b8ad50d05
robert jakub <rj@project2.pl>
2025-12-03 10:49:59 +0100

    Update 7.0.nix

--- pkgs/graylog-enterprise/7.0.nix
+++ pkgs/graylog-enterprise/7.0.nix
@@ -3,7 +3,7 @@ let buildGraylog = callPackage ./graylog-enterprise.nix { };
 in
 buildGraylog {
   version = "7.0.1";
-  hash = "sha256-sXBNY9Tf5sqg3zRgyv7+ErAenyPsIGynMwfg3VoQn00=";
+  hash = "sha256-10b7Bq6HBDDq+dfzUiKeq81EtYhq8tfASSFccQsotS4=";
   maintainers = with lib.maintainers; [ bbenno ];
   license = lib.licenses.sspl;
 }
